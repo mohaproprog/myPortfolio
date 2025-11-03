6ef6e8c3e9230126a820a29d9565ceac2b3e89af
mohaproprog <mohaproprog@gmail.com>
2025-11-03 17:41:57 +0000

    added about me links

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,5 +1,7 @@
 import './about.css'
 import about from '../assets/about.jpg'
+import fakeCv from '../assets/fakeCv.pdf';
+import { Link } from 'react-scroll';
 
 
 function About() {
@@ -14,8 +16,8 @@ Skilled in React.js, JavaScript, HTML, CSS, and Tailwind for building modern web
 I also work with Node.js to bring dynamic functionality to the backend.
 Design matters to me—Figma helps me craft intuitive and visually appealing layouts.
 Constantly learning and improving to turn ideas into interactive digital products</p>
-<button className='cv'>Download Cv <i className="fa-solid fa-chevron-down"></i></button>
-<button>Contact me <i className="fa-solid fa-reply"></i></button>
+<button className='cv'><a href={fakeCv} download>Download Cv</a> <i className="fa-solid fa-chevron-down"></i></button>
+<button><Link to="contact" smooth={true} duration={500} spy={true} offset={-100}>Contact me <i className="fa-solid fa-reply"></i></Link></button>
         </div>
     </div>
   )
